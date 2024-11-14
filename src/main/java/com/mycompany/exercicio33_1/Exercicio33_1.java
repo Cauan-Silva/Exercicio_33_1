d97aedafe8a7513f54fc5d835eae1f04b1c0afcc
Cauan-Silva <anacauansantos@gmail.com>
2024-11-14 19:08:05 -0300

    .

---
+++ src/main/java/com/mycompany/exercicio33_1/Exercicio33_1.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.exercicio33_1;
+
+/**
+ *
+ * @author LoLvSk
+ */
+public class Exercicio33_1 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
